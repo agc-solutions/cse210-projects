24402b2c2e7e3e9009df54dde5ab890db999b195
agc-solutions <agc@agcsolutions.com.br>
2023-04-20 19:18:40 -0300

    my 1st task

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+        // Ask the user for their name.
+
+        Console.Write("\r\nWhat is your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        
+        Console.WriteLine($"\r\nYour name is {last_name}, {first_name} {last_name}.\r\n");
     }
 }
